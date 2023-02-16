a1661de580d15a46b220da57aa2035b127013b8c
Leonardo Andrade <carlos.andrade@coppel.com>
2023-02-16 17:44:18 -0600

    se cargan archivos del repositorio local al remoto

---
+++ src/main/java/com/javaplatzispring/app/JavaplatzispringApplication.java
@@ -0,0 +1,13 @@
+package com.javaplatzispring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaplatzispringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaplatzispringApplication.class, args);
+	}
+
+}
